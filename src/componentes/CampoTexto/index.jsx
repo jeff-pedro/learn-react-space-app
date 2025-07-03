0cee8e017f190d009cabcac497879a22e05cb924
Jefferson Pedro <jeffersonpedro89@gmail.com>
2025-07-03 13:17:15 -0300

    adicionando cabecalho

---
+++ src/componentes/CampoTexto/index.jsx
@@ -0,0 +1,42 @@
+import styled from "styled-components"
+import search from "../../assets/search.png"
+
+const ContainerEstilizado = styled.div`
+  position: relative;
+  display: inline-block;
+`
+
+const CampoTextoEstilizado = styled.input`
+    height: 56px;
+    padding: 0.75rem 1rem;
+    border-radius: 10px;
+    border: 2px solid;
+    border-color: #C98CF1;
+    background: transparent;
+    box-sizing: border-box;
+    width: 566px;
+    color: #D9D9D9;
+    font-weight: 400;
+    font-size: 1.25rem;
+    line-height: 1.25rem;
+`
+
+const IconeLupa = styled.img`
+  position: absolute;
+  right: 1rem;
+  bottom: 50%;
+  transform: translateY(25%);
+  width: 38px;
+  height: 38px;
+`
+
+const CampoTexto = (props) => {
+  return (
+    <ContainerEstilizado>
+      <CampoTextoEstilizado placeholder="O que você procura?" {...props} />
+      <IconeLupa src={search} />
+    </ContainerEstilizado>
+  )
+}
+
+export default CampoTexto
