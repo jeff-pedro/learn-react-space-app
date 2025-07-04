b2f81afe88cb74a0d8bb7c0a0df7c3786c3e2658
Jefferson Pedro <jeffersonpedro89@gmail.com>
2025-07-03 23:47:41 -0300

    estilizando itens de lista e adicionando fontes

--- src/componentes/BarraLateral/ItemNavegacao/index.jsx
+++ src/componentes/BarraLateral/ItemNavegacao/index.jsx
@@ -1,12 +1,20 @@
 import styled from "styled-components"
 
 const ItemDeListaEstilizado = styled.li`
-  text-decoration: none;
+  display: flex;
+  align-items: center;
+  gap: 18px;
+  margin-bottom: 30px;
+  font-size: 24px;
+  line-height: 29px;
+  color: ${ (props) => props.$ativo ? "#7B78E5" : "#D9D9D9" };
+  font-family: ${ (props) => props.$ativo ? "GandhiSansBold" : "GandhiSansRegular" };
+  cursor: pointer;
 `
 
 const ItemNavegacao = ({ children, iconeAtivo, iconeInativo, ativo = false }) => {
   return (
-    <ItemDeListaEstilizado>
+    <ItemDeListaEstilizado $ativo={ativo}>
       <img src={ ativo ? iconeAtivo : iconeInativo } alt="" />    
       {children}
     </ItemDeListaEstilizado>
