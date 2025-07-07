52cc9f00cd1a5e16d319b4d61075e572c60dca42
Jefferson Pedro <jeffersonpedro89@gmail.com>
2025-07-07 11:16:11 -0300

    estruturando componente de imagem

---
+++ src/componentes/Galeria/Imagem/index.jsx
@@ -0,0 +1,18 @@
+const Imagem = () => {
+  return (
+    <figure>
+      <img src="" alt="" />
+      <p>Tag</p>
+      <figcaption>
+        <footer>
+            <h3>Titulo</h3>
+            <p>Fonte/examplo/satélite</p>
+            <button>Favoritos</button>
+            <button>Expandido</button>
+        </footer>
+      </figcaption>
+    </figure>
+  )
+}
+
+export default Imagem
--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -5,20 +5,24 @@ import Tags from "./Tags"
 
 const GaleriaContainer = styled.div`
   display: flex;
-`
+  `
 
 const SecaoFluida = styled.section`
   flex-grow: 1;
-`
+  `
 
-const Galeria = () => {
+const Galeria = ({ fotos }) => {
   return (
     <>
       <Tags />
       <GaleriaContainer>
         <SecaoFluida>
           <Titulo>Navegue pela galeria</Titulo>
-
+            {fotos.map(foto => 
+              <ul key={foto.id}>
+                <li>{foto.titulo}</li>
+              </ul>
+            )}
         </SecaoFluida>
         <Populares />
       </GaleriaContainer>
