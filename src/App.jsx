6d82d88cdc939e28d39e037958544514397b4fb7
Jefferson Pedro <jeffersonpedro89@gmail.com>
2025-07-07 10:59:19 -0300

    importando fotos

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import styled from "styled-components"
 import EstilosGlobais from "./componentes/EstilosGlobais"
 import Cabecalho from "./componentes/Cabecalho"
@@ -5,6 +6,8 @@ import BarraLateral from "./componentes/BarraLateral"
 import Banner from "./componentes/Banner"
 import banner from "./assets/banner.png"
 import Galeria from "./componentes/Galeria"
+
+import fotos from "./fotos.json"
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -29,7 +32,9 @@ const ConteudoGaleria = styled.section`
   flex-grow: 1;
 `
 
-function App() {
+const App = () => {
+  const [fotosDaGaleria, setFotosDaGaleria] = useState(fotos)
+
   return (
     <FundoGradiente>
       <EstilosGlobais />
@@ -42,7 +47,7 @@ function App() {
               texto="A galeria mais completa de fotos do espaço!"
               backgroundImage={banner}
             />
-            <Galeria />
+            <Galeria fotos={fotosDaGaleria} />
           </ConteudoGaleria>
         </MainContainer>
       </AppContainer>
